b6a35d446c695852c444009cee7f5cfa3e455410
Deathkael83 <135058800+Deathkael83@users.noreply.github.com>
2025-11-15 03:31:49 +0100

    Update remote-reload-listener.js

--- scripts/remote-reload-listener.js
+++ scripts/remote-reload-listener.js
@@ -2,10 +2,14 @@
 // Ogni client ascolta il comando "reload" dal GM e ricarica la pagina.
 
 Hooks.once("ready", () => {
-  // Sostituisci con l'ID reale del tuo modulo, identico a quello in module.json
+  // ID del modulo: DEVE essere uguale a quello in module.json
   const MODULE_ID = "wfrp4e-chargen-gm-only";
 
+  console.log(`[${MODULE_ID}] Reload listener inizializzato per`, game.user.name);
+
   game.socket.on(`module.${MODULE_ID}`, data => {
+    console.log(`[${MODULE_ID}] Messaggio socket ricevuto su client`, game.user.name, data);
+
     if (!data || data.action !== "reload") return;
 
     ui.notifications.info("Il GM ha aggiornato l'interfaccia. Ricarico la pagina...");
